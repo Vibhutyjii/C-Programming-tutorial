946e655bfe2466c9b15dd8f5d05e9dd8a02bc615
Vibhuty jii <vibhusahu2003@gmail.com>
2022-11-06 20:24:19 +0530

    0002 Second commit of the day

---
+++ day.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main() {
+    int a,b;
+
+    printf("Enter a : \n");
+    scanf("%d",&a);
+
+    printf("Enter b : \n");
+    scanf("%d",&b);
+
+    printf("Sum of A & b is - %d \n", a + b);
+
+    return 0;
+}
---
+++ day1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main() {
+    int number = 25;
+    char star = '*';
+    int age = 22;
+    float pi = 3.14;
+
+    printf("%d \n",number);
+    printf("%c \n",star);
+    printf("%d \n",age);
+    printf("%f \n",pi);
+
+    return 0;
+}
